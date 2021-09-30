6caa0b466edc1c55a642b6f300e5831d27681404
JCMilito <jcm2112@gmail.com>
2021-09-30 15:56:58 -0300

    Fix broken email address

---
+++ pages/teste.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from 'next';
+import { useRouter } from "next/router";
+
+const Update: NextPage = () => {
+  const router = useRouter();
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default Update;
